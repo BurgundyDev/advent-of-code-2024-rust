94b18a8d1d66fab58b631b30fb1c304c4e517811
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2024-12-04 09:17:54 +0000

    Day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,11 +1,11 @@
+use std::str::Chars;
+
 use regex::Regex;
 
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let char_map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let xmas_regex = Regex::new(r"XMAS").unwrap();
-    let samx_regex = Regex::new(r"SAMX").unwrap();
     let mut result: u32 = 0;
     for (li, line) in char_map.iter().enumerate()
     {
@@ -13,46 +13,106 @@ pub fn part_one(input: &str) -> Option<u32> {
         {
             if *char == 'X'
             {
-                if ci >= 4
+                if ci >= 3
                 {
-                    // check left
-                    if li >= 4
+                    if line[ci-1] == 'M'
+                    && line[ci-2] == 'A'
+                    && line[ci-3] == 'S'
                     {
-                        // check left-up
+                        result += 1
+                    }
+                    if li >= 3
+                    {
+                        if char_map[li-1][ci-1] == 'M'
+                        && char_map[li-2][ci-2] == 'A'
+                        && char_map[li-3][ci-3] == 'S'
+                        {
+                            result += 1
+                        }
                     }
                     if li + 3 < char_map.len()
                     {
-                        // check left-down
+                        if char_map[li+1][ci-1] == 'M'
+                        && char_map[li+2][ci-2] == 'A'
+                        && char_map[li+3][ci-3] == 'S'
+                        {
+                            result += 1
+                        }
                     }
                 }
                 if ci + 3 < line.len()
                 {
-                    // check right
-                    if li >= 4
+                    if line[ci+1] == 'M'
+                    && line[ci+2] == 'A'
+                    && line[ci+3] == 'S'
                     {
-                        // check right-up
+                        result += 1
+                    }
+                    if li >= 3
+                    {
+                        if char_map[li-1][ci+1] == 'M'
+                        && char_map[li-2][ci+2] == 'A'
+                        && char_map[li-3][ci+3] == 'S'
+                        {
+                            result += 1
+                        }
                     }
                     if li + 3 < char_map.len()
                     {
-                        // check right-down
+                        if char_map[li+1][ci+1] == 'M'
+                        && char_map[li+2][ci+2] == 'A'
+                        && char_map[li+3][ci+3] == 'S'
+                        {
+                            result += 1
+                        }
                     }
                 }
-                if li >= 4
+                if li >= 3
                 {
-                    // check up
+                    if char_map[li-1][ci] == 'M'
+                    && char_map[li-2][ci] == 'A'
+                    && char_map[li-3][ci] == 'S'
+                    {
+                        result += 1
+                    }
                 }
                 if li + 3 < char_map.len()
                 {
-                    // check down
+                    if char_map[li+1][ci] == 'M'
+                    && char_map[li+2][ci] == 'A'
+                    && char_map[li+3][ci] == 'S'
+                    {
+                        result += 1
+                    }
                 }
             }
         }
     }
-    None
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let char_map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut result: u32 = 0;
+    for (li, line) in char_map.iter().enumerate()
+    {
+        for (ci, char) in line.iter().enumerate()
+        {
+            if *char == 'A' && ci > 0 && ci + 1 < line.len() && li > 0 && li + 1 < char_map.len()
+            {
+                let chars: Vec<char> = vec![char_map[li-1][ci-1], char_map[li-1][ci+1],
+                                            char_map[li+1][ci+1], char_map[li+1][ci-1]];
+                if chars == ['M', 'M', 'S', 'S']
+                || chars == ['M', 'S', 'S', 'M']
+                || chars == ['S', 'S', 'M', 'M']
+                || chars == ['S', 'M', 'M', 'S']
+                {
+                    result += 1
+                }
+            }
+        }
+    }
+    Some(result)
 }
 
 #[cfg(test)]
@@ -62,12 +122,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(18));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
