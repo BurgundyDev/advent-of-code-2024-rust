5484827f440dc79dafd5a0e17a240d50e23132f9
Tolly Kulczycki <tolly.kulczycki@protonmail.com>
2024-12-08 15:43:12 +0100

    Day6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,8 +1,8 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, result};
 
 advent_of_code::solution!(6);
 
-#[derive(PartialEq, PartialOrd, Debug)]
+#[derive(Eq, Hash, PartialEq, PartialOrd, Debug, Clone, Copy)]
 pub enum Direction {
     Up,
     Right,
@@ -83,7 +83,108 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let labirynth: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    // (y, x) format
+    let mut start_point: (usize, usize) = (0, 0);
+    let mut curr_point: (usize, usize)= (0, 0);
+    let mut next_point: (usize, usize) = (0, 0);
+    let mut direction: Direction = Direction::Up;
+    for (ri, row) in labirynth.iter().enumerate()
+    {
+        for (ci, char) in row.iter().enumerate()
+        {
+            if *char == '^'
+            {
+                start_point = (ri, ci);
+                curr_point = (ri, ci);
+                next_point = (ri - 1, ci);
+            }
+        }
+    }
+    let mut labirynt_move_map: HashSet<(usize, usize)> = HashSet::new();
+    labirynt_move_map.insert(curr_point);
+    while next_point.0 < labirynth.len() && next_point.1 < labirynth[0].len()
+    {
+        // println!("{:?}", curr_point);
+        // println!("{:?}", next_point);
+        match labirynth[next_point.0][next_point.1] {
+            '#' => {
+                direction = rotate(direction);
+                match direction {
+                    Direction::Up => next_point = (curr_point.0 - 1, curr_point.1),
+                    Direction::Right => next_point = (curr_point.0, curr_point.1 + 1),
+                    Direction::Down => next_point = (curr_point.0 + 1, curr_point.1),
+                    Direction::Left => next_point = (curr_point.0, curr_point.1 - 1),
+                }
+            }
+            _ => {
+                curr_point = next_point;
+                labirynt_move_map.insert(curr_point);
+                if (curr_point.0 == 0 && direction == Direction::Up) || (curr_point.1 == 0 && direction == Direction::Left) { break; }
+                match direction {
+                    Direction::Up => next_point = (curr_point.0 - 1, curr_point.1),
+                    Direction::Right => next_point = (curr_point.0, curr_point.1 + 1),
+                    Direction::Down => next_point = (curr_point.0 + 1, curr_point.1),
+                    Direction::Left => next_point = (curr_point.0, curr_point.1 - 1),
+                }
+            }
+        }
+    }
+    for (ri, row) in labirynth.iter().enumerate()
+    {
+        for (ci, char) in row.iter().enumerate()
+        {
+            if labirynt_move_map.contains(&(ri, ci))
+            {
+                print!("X")
+            } else {
+                print!("{}", char)
+            }
+        }
+        println!()
+    }
+
+    let mut result: u32 = 0;
+    let mut modifiable_labirynth = labirynth.clone();
+    for position in labirynt_move_map.iter()
+    {
+        if *position == start_point || modifiable_labirynth[position.0][position.1] == '#' { continue; }
+        modifiable_labirynth[position.0][position.1] = '#';
+
+        curr_point = start_point;
+        next_point = (start_point.0 - 1, start_point.1);
+
+        direction = Direction::Up;
+
+        let mut local_move_map: HashSet<(usize, usize, Direction)> = HashSet::new();
+        loop
+        {
+            if local_move_map.contains(&(curr_point.0, curr_point.1, direction))
+            {
+                result += 1;
+                break;
+            }
+            local_move_map.insert((curr_point.0, curr_point.1, direction));
+
+            if (curr_point.0 == 0 && direction == Direction::Up) || (curr_point.1 == 0 && direction == Direction::Left) { break; }
+            match direction {
+                Direction::Up => next_point = (curr_point.0 - 1, curr_point.1),
+                Direction::Right => next_point = (curr_point.0, curr_point.1 + 1),
+                Direction::Down => next_point = (curr_point.0 + 1, curr_point.1),
+                Direction::Left => next_point = (curr_point.0, curr_point.1 - 1),
+            }
+            if next_point.0 >= modifiable_labirynth.len() || next_point.1 >= modifiable_labirynth[0].len() { break; }
+
+            if modifiable_labirynth[next_point.0][next_point.1] == '#'
+            {
+                direction = rotate(direction);
+            } else {
+                curr_point = next_point;
+            }
+        }
+        modifiable_labirynth[position.0][position.1] = '.';
+    }
+    Some(result)
 }
 
 #[cfg(test)]
@@ -99,6 +200,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
